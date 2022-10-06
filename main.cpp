7333cf0a7b574599a14e67d730f917a0c92898b9
anitaprova <104943181+anitaprova@users.noreply.github.com>
2022-10-06 10:40:41 -0400

    finished

--- main.cpp
+++ main.cpp
@@ -4,10 +4,20 @@
 using namespace std;
   
 void sort2(int& a, int& b){
-  
+	int temp = a;
+	a = b;
+	b = temp;
+	cout << a << " " << b;
 }
   
 int main() {
+	int u = 0;
+	cin >> u;
+
+	int v = 0;
+	cin >> v;
+
+	sort2(u, v);
 
 	return 0;
 }
